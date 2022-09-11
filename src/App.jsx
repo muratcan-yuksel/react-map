24a57766c944f7e1e33574030799fee8edec9c5b
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-09-12 01:34:28 +0300

    fallback

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,8 @@ import capitals from "./capitals.json";
 
 const App = () => {
   const [content, setContent] = useState("");
-  // const [countryName, setCountryName] = useState({});
+  const [countryName, setCountryName] = useState("");
+  const [chosenCountry, setChosenCountry] = useState("");
   // const [country, setCountry] = useState("");
   const worldMap =
     "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json";
@@ -28,7 +29,9 @@ const App = () => {
       (e) => e.properties.iso3.toString() == content.toString()
     );
     console.log(result);
+    setChosenCountry(result);
     console.log(worldCities);
+    console.log(chosenCountry[0].geometry);
     // let arr = [];
     // worldCities.map((city) => arr.push(city.country));
     // console.log(arr);
@@ -38,7 +41,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>lets build</h1>
-      <ReactTooltip>{content} </ReactTooltip>
+      <ReactTooltip>{countryName} </ReactTooltip>
       <div className="mapContainer">
         {/* projection="geoAzimuthalEqualArea" can be added to ComposableMap to change the map projection */}
         <ComposableMap data-tip="">
@@ -57,7 +60,7 @@ const App = () => {
                       // const { NAME } = e.properties;
                       setContent(e.id);
                       console.log(e);
-                      // setCountryName(e.properties);
+                      setCountryName(e.properties.name);
                     }}
                     onMouseLeave={() => {
                       setContent("");
@@ -76,19 +79,32 @@ const App = () => {
               }
             </Geographies>
             {/* markers */}
-            {/* {worldCities.splice(25, 46).map(({ name, lat, lng, country }) => {
+            {/* {worldCities.features.map(({ geometry, properties }) => {
               return (
-                <Marker key={lat} coordinates={[lat, lng]}>
+                <Marker
+                  key={geometry.coordinates}
+                  coordinates={geometry.coordinates}
+                >
                   <circle r={5} fill="yellow" stroke="white" strokeWidth={2} />
                   <text
                     textAnchor="middle"
                     style={{ fontFamily: "system-ui", fill: "orange" }}
                   >
-                    {name}
+                    {properties.city}
                   </text>
                 </Marker>
               );
             })} */}
+
+            <Marker coordinates={[37.35, 55.45]}>
+              <circle r={10} fill="yellow" stroke="white" strokeWidth={2} />
+              <text
+                textAnchor="middle"
+                style={{ fontFamily: "system-ui", fill: "orange" }}
+              >
+                lool
+              </text>
+            </Marker>
             {/* <Annotation
               subject={[worldCities[50].lat, worldCities[49].lng]}
               dx={-90}
