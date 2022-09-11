37de783769f25be94a9e941f0e940a1b4af97866
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-09-12 00:32:02 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,29 @@ import cities from "cities.json";
 
 const App = () => {
   const [content, setContent] = useState("");
+  const [alpha, setAlpha] = useState({});
+  const [country, setCountry] = useState("");
   const worldMap =
     "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json";
 
   const worldCities = cities;
   console.log(worldCities);
+
+  // const searchCountry=()=>{
+  //   worldCities.filter(e=>e.properties.Alpha-2)
+  // }
+  //need to get country abbreviation
+  //but for some reason, the returned format is not chooseable(or I don't know how)
+  //So I loop over the object at question in geographies (which is a country)
+  //and get the country abbreviation from there
+  const getCountryAbbr = () => {
+    for (const [key, value] of Object.entries(alpha)) {
+      console.log(`${key}: ${value}`);
+      console.log(value);
+      setCountry(value);
+    }
+  };
+
   return (
     <div className="App">
       <h1>lets build</h1>
@@ -40,10 +58,14 @@ const App = () => {
                     onMouseEnter={() => {
                       // const { NAME } = e.properties;
                       setContent(e.properties.name);
-                      console.log(e);
+                      console.log(e.properties);
+                      setAlpha(e.properties);
                     }}
                     onMouseLeave={() => {
                       setContent("");
+                    }}
+                    onClick={() => {
+                      getCountryAbbr();
                     }}
                     style={{
                       hover: {
